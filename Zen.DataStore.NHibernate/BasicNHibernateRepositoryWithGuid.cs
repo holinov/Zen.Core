282f0b34890a2814c939de2c232cfbe95db41c9d
holinov <holinov@netvoxlab.ru>
2013-10-24 10:40:46 +0400

    Начало работк по NHibernate и SQLite

---
+++ Zen.DataStore.NHibernate/BasicNHibernateRepositoryWithGuid.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NHibernate;
+
+namespace Zen.DataStore.NHibernate
+{
+    public class BasicNHibernateRepositoryWithGuid<TEntity> : BasicNHibernateRepository<TEntity>,
+                                                              IRepositoryWithGuid<TEntity>
+        where TEntity : class, IHasGuidId
+    {
+        public BasicNHibernateRepositoryWithGuid(ISession session) : base(session)
+        {
+        }
+
+        public BasicNHibernateRepositoryWithGuid(ISession session, ITransaction transaction) : base(session, transaction)
+        {
+        }
+
+        /// <summary>
+        ///     Найти объект по GUID
+        /// </summary>
+        /// <param name="guid">Уникальный ИД объекта</param>
+        /// <returns></returns>
+        public TEntity Find(Guid guid)
+        {
+            return Query.First(e => e.Guid == guid);
+        }
+
+        public IQueryable<TEntity> Find(IEnumerable<Guid> guids)
+        {
+            return Query.Where(e => guids.Contains(e.Guid));
+        }
+
+        /// <summary>
+        ///     Клонировать документ
+        /// </summary>
+        /// <param name="entity">Документ, который нужно клонировать</param>
+        public void Clone(TEntity entity)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ Zen.DataStore.NHibernate/NHibernateDatastoreModule.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+using Module = Autofac.Module;
+
+namespace Zen.DataStore.NHibernate
+{
+    public class NHibernateDatastoreModule:Module
+    {
+        private readonly Assembly[] _mappingAssemblies;
+        private readonly IPersistenceConfigurer _persConf;
+
+        public NHibernateDatastoreModule(params Assembly[] mappingAssemblies):this(null,mappingAssemblies)
+        {
+            
+        }
+        public NHibernateDatastoreModule(IPersistenceConfigurer persConf, Assembly[] mappingAssemblies)
+        {
+            _persConf = persConf;
+            _mappingAssemblies = mappingAssemblies;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(c => BuildSessionFactory())
+                   .As<ISessionFactory>()
+                   .OnRelease(s =>
+                       {
+                           if (!s.IsClosed) s.Close();
+                       })
+                   .SingleInstance();
+            builder.Register(c => c.Resolve<ISessionFactory>().OpenSession()).As<ISession>().InstancePerDependency();
+        }
+
+        private ISessionFactory BuildSessionFactory()
+        {
+            var cnf = Fluently.Configure();
+            cnf.Database(_persConf ?? SQLiteConfiguration.Standard.UsingFile("data.db"));
+            cnf.Mappings(m =>
+                {
+                    foreach (var mappingAssembly in _mappingAssemblies)
+                    {
+                        m.FluentMappings.AddFromAssembly(mappingAssembly);
+                    }
+                });
+            cnf.ExposeConfiguration(config => new SchemaExport(config).Create(false, true));
+
+            return cnf.BuildSessionFactory();
+        }
+    }
+}
