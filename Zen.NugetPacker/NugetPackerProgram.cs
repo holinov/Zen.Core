ab88332ac96323b23b0dbaacdb4a7bf3a64faade
holinov <holinov@netvoxlab.ru>
2013-11-12 16:20:32 +0400

    Оптимизация Refrence

--- Zen.NugetPacker/NugetPackerProgram.cs
+++ Zen.NugetPacker/NugetPackerProgram.cs
@@ -27,7 +27,7 @@ namespace Zen.NugetPacker
                     {
                         BuildType = "Release",
                         SolutionPath = "C:\\src\\Zen.Core",
-                        VersionString = "1.1.0.44"
+                        VersionString = "1.1.0.45"
                     };
                 packageList = new List<NugetPackage>()
                     {
