c6ce6cbe5798d8b7ad458054166c32a286b6f5b1
holinov <holinov@netvoxlab.ru>
2013-12-24 15:54:40 +0400

    Версия

--- Zen.NugetPacker/NugetPackerProgram.cs
+++ Zen.NugetPacker/NugetPackerProgram.cs
@@ -27,7 +27,7 @@ namespace Zen.NugetPacker
                     {
                         BuildType = "Release",
                         SolutionPath = "C:\\src\\Zen.Core",
-                        VersionString = "1.1.0.55"
+                        VersionString = "1.1.0.56"
                     };
                 packageList = new List<NugetPackage>()
                     {
